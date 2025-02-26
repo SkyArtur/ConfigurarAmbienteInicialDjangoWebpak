80f636eb9c93db7876e8dd53d3620642b7e00fc2
Artur dos Santos Shon <sky_artur@hotmail.com>
2025-02-25 21:52:35 -0300

    Initial commit.

--- config/InitialConfigProjectScript.bat
+++ config/InitialConfigProjectScript.bat
@@ -1,8 +1,10 @@
 :: Desativa a exibição dos comandos no console para uma aparência mais limpa.
 @echo off
 setlocal EnableDelayedExpansion
+
 :: Caminho para o arquivo .env
 set ENV_FILE=.\files\.env
+
 :: Verifica se o arquivo .env existe
 if exist %ENV_FILE% (
     for /f "usebackq tokens=1,* delims== " %%A in (%ENV_FILE%) do (
@@ -12,41 +14,62 @@ if exist %ENV_FILE% (
     echo Arquivo .env não encontrado.
     exit /b 1
 )
-:: Altera o diretório atual para o diretório onde o script está localizado.
+
+:: Altera o diretório para onde o script está localizado e sobe um nível
 cd /d %~dp0
-:: Retorna o prompt para o diretório que será a raiz do projeto.
 cd ..
+
 :: cria um ambiente virtual Python para o projeto.
 call python -m venv .venv
+
 :: iniciar o ambiente virtual do projeto.
 call .\.venv\Scripts\activate.bat
+
 :: Atualiza o pip.
 call python -m pip install --upgrade pip
+
 :: instalar as bibliotecas python necessárias.
 call pip install django pillow python-dotenv passlib psycopg2 psycopg2-binary
+
 :: Inicia o NodeJS.
 call npm init -y
+
 :: Instala os módulos necessários.
-call npm i  --save-dev webpack webpack-cli babel-loader @babel/core @babel/preset-env css-loader sass-loader sass style-loader mini-css-extract-plugin source-map-loader css-minimizer-webpack-plugin terser-webpack-plugin bootstrap bootstrap-icons jquery dotenv
-:: Inicia o projeto Django.
-call django-admin startproject setup .
-:: Cria o app.
-call python .\manage.py startapp %APP%
+call npm i  --save-dev webpack webpack-cli babel-loader @babel/core @babel/preset-env ^
+    css-loader sass-loader sass style-loader mini-css-extract-plugin source-map-loader ^
+    css-minimizer-webpack-plugin terser-webpack-plugin bootstrap bootstrap-icons jquery dotenv
+
 :: Cria as pastas iniciais do projeto e para o desenvolvimento.
 mkdir templates\pages templates\includes src\js src\sass  %APP%\static\css  %APP%\static\js
+
 :: Cria os arquivos iniciais para desenvolvimento frontend.
 echo.>src\js\index.js
 echo.>src\sass\main.sass
 echo.>.gitignore
 echo.>templates\index.html
+
 :: Move os módulos necessários para a pasta 'static' do projeto.
 move node_modules\bootstrap\dist %APP%\static\bootstrap
 move node_modules\bootstrap-icons\font %APP%\static\bootstrap\icons
 move node_modules\jquery\dist %APP%\static\jquery
+
 :: Copia os arquivos para a raiz do projeto.
 if exist config\files\.env copy config\files\.env .\.env
 if exist config\files\webpack.config.js copy config\files\webpack.config.js .\webpack.config.js
 if exist config\files\docker-compose.yaml copy config\files\docker-compose.yaml .\docker-compose.yaml
+
+GOTO APLICACAO
+:: Este bloco de código é ignorado pela execução do script
+:: Caso deseje criar a aplicação automaticamente, remova GOTO APLICACAO e :APLICACAO
+:: Inicia o projeto Django.
+call django-admin startproject setup .
+:: Cria o app.
+call python .\manage.py startapp %APP%
+:APLICACAO
+
+GOTO DOCKERCOMPOSE
+:: Este bloco de código é ignorado pela execução do script
+:: Caso deseje configurar o container para o banco de dados da aplicação, remova GOTO DOCKERCOMPOSE e :DOCKERCOMPOSE
 :: Verifica se o Docker está rodando.
 call docker info >null 2>&1
 if %ERRORLEVEL% NEQ 0 (
@@ -55,5 +78,7 @@ if %ERRORLEVEL% NEQ 0 (
     :: inicia a configuração do container do banco de dados da aplicação.
     call docker-compose up -d
 )
+:DOCKERCOMPOSE
+
 echo "Ambiente configurado."
 pause
